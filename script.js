ab0f777414b9be6ade36b615e0db9b4d0e32b283
Radik Abubjazov <abubjazov@mail.ru>
2021-07-13 23:34:23 +0300

    циклы в JS

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert(1);
+let obj = {
+    dog: 'Puppy',
+    cat: 'Murr'
+};
+
+let answers = ['Ваше имя: ', 'Ваша фамилия: ', 'Ваш возраст: '];
+
+for (let i = 0; i < answers.length; i++) {
+    answers[i] = prompt(answers[i], '');
+    console.log(answers[i]);
+}
+
+console.log(answers);
+answers = ['Ваше имя: ', 'Ваша фамилия: ', 'Ваш возраст: '];
+console.log(answers);
+
+let i = 0;
+do {
+    answers[i] = prompt(answers[i], '');
+    console.log(answers[i]);
+    i++;
+} while (i < answers.length);
+
+console.log(answers);
+answers = ['Ваше имя: ', 'Ваша фамилия: ', 'Ваш возраст: '];
+console.log(answers);
+
+i = 0;
+while (i < answers.length) {
+    answers[i] = prompt(answers[i], '');
+    console.log(answers[i]);
+    i++;
+}
+
+console.log(answers);

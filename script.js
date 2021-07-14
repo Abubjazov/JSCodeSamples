5f55a55a7d14b5e9d7fd852923336ef9a9d995eb
Radik Abubjazov <abubjazov@mail.ru>
2021-07-14 14:52:37 +0300

    функции в JS

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let obj = {
-    dog: 'Puppy',
-    cat: 'Murr'
-};
+//Циклы
 
 let answers = ['Ваше имя: ', 'Ваша фамилия: ', 'Ваш возраст: '];
 
@@ -33,3 +30,62 @@ while (i < answers.length) {
 }
 
 console.log(answers);
+
+//Функции
+
+//function declaration
+function sayHello(user) {
+    console.log('Hello ' + user + '!');
+}
+
+i = 'Master';
+
+sayHello(i);
+
+
+//function declaration
+function calc2(a, b) {
+    return a + b;
+}
+
+console.log(calc2(13, Number('5')));
+
+
+//function expression
+let calc = function(a, b) {
+    console.log(a + b);
+};
+
+calc(13, Number('5'));
+
+
+function myFistFunction(name, age) {
+    console.log(`Hello, my name is ${name} and this is my first function!`);
+
+    function showSkills() {
+        let skills = ['HTML5', 'CSS3', 'JS', 'GIT', 'Python', 'SQL'];
+        console.log(`Я владею: `);
+        for (let i = 0; i < skills.length; i++) {
+            console.log(skills[i]);
+        }
+    }
+
+    function checkAge() {
+        if (Number(age) > 18) {
+            console.log(`${name} у тебя отличные шансы стать tru'шным FrontEnd'ером`);
+        } else {
+            console.log(`${name} ты молодец что задумался о своём будущем! 
+            Вперёд и только вперёд у тебя обязательно всё получится!!!`);
+        }
+    }
+
+    function calcPow(number) {
+        return Number(number)**2;
+    }
+
+    showSkills();
+    checkAge();
+    console.log(calcPow(9));
+}
+
+myFistFunction('Radik', '20');

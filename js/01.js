006225102ef807706a5ccc03f3b0f98648215500
Radik Abubjazov <abubjazov@mail.ru>
2021-09-11 17:59:19 +0300

    базовые операторы JS и особенности их применения

---
+++ js/01.js
@@ -0,0 +1,21 @@
+// asi
+const num1 = 13,
+      num2 = 7;
+
+console.log('+', num1 + num2);
+console.log('-', num1 - num2);
+console.log('*', num1 * num2);
+console.log('/', num1 / num2);
+
+const str1 = 'Hello',
+      str2 = 'World';
+
+console.log('+', str1 + ' ' + str2);
+console.log('1 + "2":', 1 + '2');
+console.log('Bool + String:', true + str1);
+
+console.log('Bool: true + 1:', true + 1);
+console.log('Bool: false + 1:', false + 1);
+
+console.log('+Bool: true:', +true);
+console.log('+Bool: false:', +false);

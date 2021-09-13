b56c821a564570367f564e807a49e03ff1c33592
Radik Abubjazov <abubjazov@mail.ru>
2021-09-13 23:06:25 +0300

    объект Math

--- js/03.js
+++ js/03.js
@@ -45,6 +45,11 @@ let array = str.split(', ');
 // });
 
 // console.log(foundPerson.name);
-console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}));
-console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}).map((i) => {return parseInt(i);}));
-console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}).map((i) => {return i * 1;}));
+// console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}));
+// console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}).map((i) => {return parseInt(i);}));
+// console.log('1, 2, 3, 4, 5, 6, 7, 8, 9'.split(', ').filter((i) => {return i % 2 !== 0;}).map((i) => {return i * 1;}));
+console.log(Math.random());
+
+let x = 2.447;
+console.log(Math.floor(x));
+console.log(Math.ceil(x));

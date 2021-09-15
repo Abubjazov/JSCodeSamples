e97c5326141aa097ea0cfa5f697a11221dfe987a
Radik Abubjazov <abubjazov@mail.ru>
2021-09-15 18:15:24 +0300

    игра Game: +renderBox()

--- js/game.js
+++ js/game.js
@@ -1,8 +1,27 @@
 let $startBtn = document.querySelector('#start');
+let $game = document.querySelector('#game');
 
 $startBtn.addEventListener('click', startGame);
 
 function startGame() {
     $startBtn.classList.add('hide');
-    document.querySelector('#game').style.background = '#fff';
+    $game.style.background = '#fff';
+    renderBox();
+}
+
+function renderBox() {
+    let box = document.createElement('div');
+    let boxTop, boxLeft, boxHeight, boxWidth, boxBackColor;
+
+    box.style.cssText = `
+                    position: absolute;
+                    top: ${boxTop}px;
+                    left: ${boxLeft}px;
+                    width: ${boxWidth}px;
+                    height: ${boxHeight}px;
+                    background: ${boxBackColor};
+                    cursor: pointer;
+                `;
+
+    $game.insertAdjacentElement('afterbegin', box);
 }

faae3b92891ce703c7231c256a401be30617d2a5
Radik Abubjazov <abubjazov@mail.ru>
2021-09-15 18:39:14 +0300

    игра Game: +gameClickHandler()

--- js/game.js
+++ js/game.js
@@ -1,27 +1,47 @@
-let $startBtn = document.querySelector('#start');
-let $game = document.querySelector('#game');
+let $startBtn = document.querySelector('#start'),
+    $game = document.querySelector('#game'),
+    $score = document.querySelector('#result'),
+    gameScore = 0;
 
 $startBtn.addEventListener('click', startGame);
+$game.addEventListener('click', gameClickHandler);
 
 function startGame() {
     $startBtn.classList.add('hide');
+    $score.parentNode.classList.remove('hide');
+    $score.textContent = 0;
     $game.style.background = '#fff';
     renderBox();
 }
 
 function renderBox() {
-    let box = document.createElement('div');
-    let boxTop, boxLeft, boxHeight, boxWidth, boxBackColor;
+    let isBox = document.querySelector('div [data-box="true"]');
 
+    if (isBox) {
+        isBox.remove();
+    }
+
+    let box = document.createElement('div'),
+        boxTop = 50, boxLeft = 50, boxSide = 70, boxBackColor = 'red' ;
+
+    box.setAttribute('data-box', 'true');
     box.style.cssText = `
                     position: absolute;
                     top: ${boxTop}px;
                     left: ${boxLeft}px;
-                    width: ${boxWidth}px;
-                    height: ${boxHeight}px;
+                    width: ${boxSide}px;
+                    height: ${boxSide}px;
                     background: ${boxBackColor};
                     cursor: pointer;
                 `;
 
     $game.insertAdjacentElement('afterbegin', box);
+}
+
+function gameClickHandler(e) {
+    if (e.target.dataset.box) {
+        $score.textContent = ++gameScore;
+        console.log($score);
+        renderBox();
+    }
 }

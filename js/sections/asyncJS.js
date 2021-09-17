65f07cd5b348bf18e30d97cd7a58f6b4ad622dd8
Radik Abubjazov <abubjazov@mail.ru>
2021-09-18 02:00:02 +0300

    асинхронность в JS: реализация на Promis'ах

--- js/sections/asyncJS.js
+++ js/sections/asyncJS.js
@@ -14,31 +14,85 @@
 /*
     Client -> Server -> Database -> Server -> Client
 */
-setTimeout(() => {
-    console.log('Client to Server: i need users list')
-    console.log('Request sending ...')
+// setTimeout(() => {
+//     console.log('Client to Server: i need users list')
+//     console.log('Request sending ...')
 
+//     setTimeout(() => {
+//         console.log('Server to Database: i need users list')
+//         console.log('Request sending ...')
+
+//         setTimeout(() => {
+//             console.log('Database: prepare users list')
+            
+//             setTimeout(() => {
+//                 console.log('Database to Server: users list')
+//                 console.log('Response sending ...')
+                
+//                 setTimeout(() => {
+//                     console.log('Server to Client: users list')
+//                     console.log('Response sending ...')
+                    
+//                     setTimeout(() => {
+//                         console.log('Client: users list')
+//                         console.log('Response received')           
+//                     }, 2000)
+//                 }, 2000)
+//             }, 2000)
+//         }, 2000)
+//     }, 2000)
+// }, 2000)
+
+
+let promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('Server to Database: i need users list')
+        console.log('Client to Server: i need users list')
         console.log('Request sending ...')
+        resolve()
+    }, 2000)
+})
 
+promise.then(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Server to Database: i need users list')
+            console.log('Request sending ...')
+            resolve()
+        }, 2000)
+    })
+})
+.then(() => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('Database: prepare users list')
-            
-            setTimeout(() => {
-                console.log('Database to Server: users list')
-                console.log('Response sending ...')
-                
-                setTimeout(() => {
-                    console.log('Server to Client: users list')
-                    console.log('Response sending ...')
-                    
-                    setTimeout(() => {
-                        console.log('Client: users list')
-                        console.log('Response received')           
-                    }, 2000)
-                }, 2000)
-            }, 2000)
-        }, 2000)
-    }, 2000)
-}, 2000)
+            resolve()
+        }, 2000)        
+    })
+})
+.then(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Database to Server: users list')
+            console.log('Response sending ...')
+            resolve()
+        }, 2000)        
+    })
+})
+.then(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Server to Client: users list')
+            console.log('Response sending ...')
+            resolve()
+        }, 2000)        
+    })
+})
+.then(() => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Client: users list')
+            console.log('Response received')
+            resolve()
+        }, 2000)        
+    })
+})

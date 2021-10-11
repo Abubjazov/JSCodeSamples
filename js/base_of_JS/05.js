d0ca3effda22089ff8fec595d44967e47e48fe00
Radik Abubjazov <abubjazov@mail.ru>
2021-10-11 21:59:13 +0300

    тема: деструктуризация объекта

--- js/base_of_JS/05.js
+++ js/base_of_JS/05.js
@@ -1,20 +1,65 @@
-function first() {
-    setTimeout(function() {
-        console.log(1)
-    }, 500)
-}
+// function first() {
+//     setTimeout(function() {
+//         console.log(1)
+//     }, 500)
+// }
 
-function second() {
-        console.log(2)
-}
+// function second() {
+//         console.log(2)
+// }
 
 // first()
 // second()
 
-function learnJS(lang, callback1, callback2) {
-    console.log(`I learn ${lang}`)
-    callback1()
-    callback2()
-}    
+// function learnJS(lang, callback1, callback2) {
+//     console.log(`I learn ${lang}`)
+//     callback1()
+//     callback2()
+// }    
 
-learnJS('JS', first, second)
+// learnJS('JS', first, second)
+
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() {
+        console.log(Object.keys(this).length)
+    }
+}
+
+// console.log(options)
+
+// delete options.name
+
+// console.log(options)
+
+let counter = 0
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let keyi in options[key]) {
+            console.log(key, keyi, options[key][keyi])   
+        }
+    } else {
+        console.log(key, options[key])
+    }
+    counter++
+}
+
+console.log(counter)
+
+console.log(Object.keys(options).length)
+console.log(Object.keys(options))
+console.log(Object.keys(options).join(' '))
+
+options.makeTest()
+
+const {border, background} = options.colors
+
+console.log(border, background)

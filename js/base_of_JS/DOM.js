355bfeb3e62b7812396604cfc153f2259616222d
Radik Abubjazov <abubjazov@mail.ru>
2021-10-14 01:08:47 +0300

    тема: DOM - действия с элементами на странице

--- js/base_of_JS/DOM.js
+++ js/base_of_JS/DOM.js
@@ -24,3 +24,32 @@ const form = document.querySelector('form')
 console.log(form)
 
 inputs.forEach(item => console.log(item))
+
+form.style.outline = '1px solid black'
+
+btns[1].style.borderRadius = '100%'
+btns[1].textContent = 'RUN'
+
+btns[2].style.cssText = 'background-color: red; width: 500px;'
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].style.borderRadius = '100%'
+}
+
+inputs.forEach(item => {
+    item.style.backgroundColor = 'green'
+})
+
+const div = document.createElement('div')
+const text = document.createTextNode('lorem ip sum')
+
+div.classList.add('added-class')
+
+// document.querySelector('form').append(div)
+// document.querySelector('form').prepend(text)
+
+btns[2].before(div)
+btns[2].after(text)
+btns[2].remove()
+
+btns[0].replaceWith(inputs[1])

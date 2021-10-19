85a0219b40cce14b9cf278d011e5703478fc402b
Radik Abubjazov <abubjazov@mail.ru>
2021-10-19 14:20:11 +0300

    тема: JSON

--- js/base_of_JS/01.js
+++ js/base_of_JS/01.js
@@ -1,23 +1,23 @@
-// const num1 = 13,
-//       num2 = 7;
+const num1 = 13,
+      num2 = 7;
 
-// console.log('+', num1 + num2);
-// console.log('-', num1 - num2);
-// console.log('*', num1 * num2);
-// console.log('/', num1 / num2);
+console.log('+', num1 + num2);
+console.log('-', num1 - num2);
+console.log('*', num1 * num2);
+console.log('/', num1 / num2);
 
-// const str1 = 'Hello',
-//       str2 = 'World';
+const str1 = 'Hello',
+      str2 = 'World';
 
-// console.log('+', str1 + ' ' + str2);
-// console.log('1 + "2":', 1 + '2');
-// console.log('Bool + String:', true + str1);
+console.log('+', str1 + ' ' + str2);
+console.log('1 + "2":', 1 + '2');
+console.log('Bool + String:', true + str1);
 
-// console.log('Bool: true + 1:', true + 1);
-// console.log('Bool: false + 1:', false + 1);
+console.log('Bool: true + 1:', true + 1);
+console.log('Bool: false + 1:', false + 1);
 
-// console.log('+Bool: true:', +true);
-// console.log('+Bool: false:', +false);
+console.log('+Bool: true:', +true);
+console.log('+Bool: false:', +false);
 
 function check(str) {
     const strArr = [...str],
--- js/base_of_JS/rest.js
+++ js/base_of_JS/rest.js
@@ -1,7 +1,36 @@
-const log = function(a, b, ...rest) {
-    console.log(a, b, rest)
+// const log = function(a, b, ...rest) {
+//     console.log(a, b, rest)
+// }
+
+// log('basic', 'rest', 'operator', 'usage'
+
+const person = {
+    name: 'Alex',
+    age: 28,
+    null: null,
+    phone: '+74443332211',
+    info: {
+        adress: 'some adress',
+        email: 'email@email.xx'
+    }
 }
 
-log('basic', 'rest', 'operator', 'usage')
+const personCP = person
+
+const personJSON = JSON.stringify(person)
+const personOBJ = JSON.parse(personJSON)
+
+const personCopy = JSON.parse(JSON.stringify(person))
+
+console.log(personJSON)
+console.log(personOBJ)
+
+console.log(person === personCopy)
+console.log(person === personCP)
+
+personCopy.info.adress = 'hhhh'
+personCP.info.adress = 'wwwww'
 
 
+console.log(person)
+console.log(personCopy)

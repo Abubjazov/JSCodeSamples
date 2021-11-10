bd6f611b903239b57d8d8b7df709236e44dc62dc
Radik Abubjazov <abubjazov@mail.ru>
2021-11-10 22:45:16 +0300

    практика: упражнение

---
+++ js/codewars/29.js
@@ -0,0 +1,23 @@
+function persistence(num) {
+    let res = []
+
+    const mulArr = (numArr) => {
+        if (numArr.length === 1) return res.length
+
+        let result = 1
+
+        numArr.forEach((elem) => {
+            result *= elem
+        })
+
+        res.push(result)
+
+        return mulArr([...result.toString()])
+    }
+
+    return mulArr([...num.toString()])
+}
+
+const num = 999
+
+console.log(persistence(num))
